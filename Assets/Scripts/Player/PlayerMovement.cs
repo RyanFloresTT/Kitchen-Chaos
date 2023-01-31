3639357d5ab966ccda624a48a36a29802a61ace5
Ryan Flores <rryanflorres@gmail.com>
2023-01-30 20:23:02 -0800

    Refactor Player Movement & Use New Input System

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,33 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private GameInput gameInput;
     private bool _isWalking;
     private void Update()
     {
-        var inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
 
+        var inputVector = gameInput.GetMovementVectorNormalized();
+        
         inputVector = inputVector.normalized;
 
         var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
