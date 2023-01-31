f84908910aa22da7e8b3b9146391f6fb9081e382
Ryan Flores <rryanflorres@gmail.com>
2023-01-30 19:51:18 -0800

    Create Player Animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float rotationSpeed = 10f;
+    private bool _isWalking;
     private void Update()
     {
         var inputVector = new Vector2(0, 0);
@@ -31,7 +32,15 @@ public class PlayerMovement : MonoBehaviour
 
         var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         var playerTransform = transform;
+
+        _isWalking = moveDir != Vector3.zero;
+        
         playerTransform.position += moveDir * (Time.deltaTime * speed);
         playerTransform.forward = Vector3.Slerp(playerTransform.forward, moveDir, Time.deltaTime * rotationSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
     }
 }
