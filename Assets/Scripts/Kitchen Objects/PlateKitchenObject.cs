5bb3f606c133ae92a2e2276c4c9fe7796de33688
Ryan Flores <rryanflorres@gmail.com>
2023-02-09 17:40:29 -0800

    Add Plate Functionality

---
+++ Assets/Scripts/Kitchen Objects/PlateKitchenObject.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateKitchenObject : KitchenObject
+{
+
+    [SerializeField] private List<KitchenObjectSO> validKitchenObjects;
+    private List<KitchenObjectSO> kitchenObjectSOList;
+
+    private void Awake()
+    {
+        kitchenObjectSOList = new List<KitchenObjectSO>();
+    }
+
+
+    public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
+    {
+        if (!validKitchenObjects.Contains(kitchenObjectSO))
+        {
+            return false;
+        }
+        if (kitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            return false;
+        }
+        kitchenObjectSOList.Add(kitchenObjectSO);
+        return true;
+    }
+} 
