9205ea161aee4c5b361ca0d8a753596f6686c3a0
Ryan Flores <rryanflorres@gmail.com>
2023-02-18 10:35:49 -0800

    Add MainMenu + Loading Logic

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool _isFirstUpdate = true;
+
+    private void Update()
+    {
+        if (_isFirstUpdate)
+        {
+            _isFirstUpdate = false;
+            
+            Loader.LoaderCallback();
+        }
+    }
+    
+
+}
